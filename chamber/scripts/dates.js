4aed95226d27aa4ddba36dd72c8e933abe15f0e0
Edward Kamwi <157596090+EddyKamwi@users.noreply.github.com>
2025-03-18 18:23:27 +0200

    design

---
+++ chamber/scripts/dates.js
@@ -0,0 +1,2 @@
+document.getElementById("last_modified").innerText = document.lastModified;
+document.getElementById("current_year").innerText = new Date().getFullYear();
---
+++ chamber/scripts/main.js
@@ -0,0 +1,62 @@
+const cards = document.getElementById("main-container");
+const list = document.getElementById("list");
+const thumbnail = document.getElementById("thumbnail");
+list.addEventListener("click", () => {
+  list.classList.toggle("button-active");
+  thumbnail.classList.toggle("button-active");
+  document.querySelectorAll("section").forEach((tag) => {
+    tag.classList.toggle("list");
+  });
+  cards.classList.toggle("main-container");
+});
+thumbnail.addEventListener("click", () => {
+  list.classList.toggle("button-active");
+  thumbnail.classList.toggle("button-active");
+  document.querySelectorAll("section").forEach((tag) => {
+    tag.classList.toggle("list");
+  });
+});
+const displayCompany = (array) => {
+  array.forEach((company) => {
+    const card = document.createElement("section");
+    const address = document.createElement("p");
+    const cell = document.createElement("p");
+    const logo = document.createElement("img");
+    const url = document.createElement("a");
+
+    // set content
+    address.textContent = `${company.address.street}, ${company.address.province}, ${company.address.country}`;
+    cell.textContent = company.cell;
+    url.setAttribute("href", company.website);
+    url.textContent = company.website;
+    logo.setAttribute("src", company.iconUrl);
+    logo.setAttribute(
+      "alt",
+      `Portrait of ${company.name} ${company.lastname} – ${company.order}${company.order} Latter-day President`
+    );
+    logo.setAttribute("loading", "lazy");
+    logo.setAttribute("height", 50);
+    logo.setAttribute("width", 120);
+
+    card.appendChild(logo);
+    card.appendChild(address);
+    card.appendChild(cell);
+    card.appendChild(url);
+
+    cards.appendChild(card);
+  });
+};
+const data = async () => {
+  const url = "http://127.0.0.1:5500/chamber/data/members.json";
+  const resp = await fetch(url);
+  const data = await resp.json();
+  displayCompany(data.companies);
+};
+
+data();
+
+// data()[2].forEach((company) => {
+//   const div = document.createComment("div");
+//   div.textContent = company;
+//   document.querySelector("main").appendChild(div);
+// });
