c0b6a6d69aec5b983cdfc4f7ba560a78da859846
Edward Kamwi <157596090+EddyKamwi@users.noreply.github.com>
2025-03-29 21:50:36 +0200

    updated json file and added some js functionality

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,20 +1,112 @@
+const dayOfWeek = (number) => {
+  const num = parseInt(number);
+  if (num === 1) {
+    return "Monday";
+  } else if (num === 2) {
+    return "Tuesday";
+  } else if (num === 3) {
+    return "Wednesday";
+  } else if (num === 4) {
+    return "Thirsday";
+  } else if (num === 5) {
+    return "Friday";
+  } else if (num === 6) {
+    return "Saturday";
+  } else if (num === 0) {
+    return "Sunday";
+  } else {
+    return "invalid day";
+  }
+};
 async function weather(url) {
   return (await fetch(url)).json();
 }
 
-const data = await weather(
-  "https://api.openweathermap.org/data/2.5/weather?lat=-15.33&lon=28.44&appid=c75f603870e497bd2c733194575b3de1&units=metric"
-);
+async function main() {
+  const data = await weather(
+    "https://api.openweathermap.org/data/2.5/weather?lat=-15.33&lon=28.44&appid=c75f603870e497bd2c733194575b3de1&units=metric"
+  );
+  const tempForcast = await weather(
+    "http://api.openweathermap.org/data/2.5/forecast?lat=-15.33&lon=28.44&appid=c75f603870e497bd2c733194575b3de1&units=metric"
+  );
+  const companies = await weather(
+    "http://127.0.0.1:5500/chamber/data/members.json"
+  );
 
-const sunrise = `${new Date(data.sys.sunrise * 1000).getHours()}:${new Date(
-  data.sys.sunrise * 1000
-).getMinutes()}hrs`;
-const sunset = `${new Date(data.sys.sunset * 1000).getHours()}:${new Date(
-  data.sys.sunset * 1000
-).getMinutes()}hrs`;
-const humidity = `${data.main.humidity}%`;
-const lowTemp = `${data.main.temp_min}°`;
-const highTemp = `${data.main.temp_max}°`;
-const descrip = `${data.weather[0].description}`;
-const temp = `${data.main.temp}°`
-console.log(temp);
+  const sunrise = `${new Date(data.sys.sunrise * 1000).getHours()}:${new Date(
+    data.sys.sunrise * 1000
+  ).getMinutes()}hrs`;
+
+  const sunset = `${new Date(data.sys.sunset * 1000).getHours()}:${new Date(
+    data.sys.sunset * 1000
+  ).getMinutes()}hrs`;
+
+  const companiesList = companies.companies;
+  const indexList = [];
+  let i = null;
+  let j = null;
+  let l = null;
+
+  while (true) {
+    i = Math.floor(Math.random() * companiesList.length);
+    j = Math.floor(Math.random() * companiesList.length);
+    l = Math.floor(Math.random() * companiesList.length);
+    if (i != j && j != l && i != l) {
+      break;
+    }
+  }
+
+  indexList.push(l);
+  indexList.push(i);
+  indexList.push(j);
+
+  const p = document.getElementById("bCard");
+  indexList.forEach((index) => {
+    const div = document.createElement("div");
+    div.setAttribute("class", "b-card");
+
+    div.innerHTML = `<h2 class="b-title">${companiesList[index].names}</h2>
+        <p class="b-tagName">${companiesList[index].tagLine}</p>
+        <hr>
+        <div class="b-body">
+          <img src="${companiesList[index].iconUrl}" alt="Logo" height="25" width="60" loading="lazy">
+          <div>
+            <p><span>Email: </span> ${companiesList[index].email}</p>
+            <p><span>Phone: </span> ${companiesList[index].cell}</p>
+            <p><span>Url: </span> ${companiesList[index].website}</p>
+          </div>
+        </div>`;
+
+    p.appendChild(div);
+  });
+
+  const humidity = `${data.main.humidity}%`;
+  const lowTemp = `${data.main.temp_min}°`;
+  const highTemp = `${data.main.temp_max}°`;
+  const descrip = `${data.weather[0].description}`;
+  const temp = `${data.main.temp}°`;
+
+  document.getElementById("c-weather-span").innerHTML = `
+      ${temp} <br>
+      ${descrip} <br>
+      high: ${highTemp}<br>
+      low: ${lowTemp}<br>
+      humidity: ${humidity}<br>
+      sunrise: ${sunrise}<br>
+      sunset: ${sunset}
+      `;
+
+  const tempToday = tempForcast.list[0].main.temp;
+
+  const tempTomorrow = tempForcast.list[8].main.temp;
+  const day = dayOfWeek(new Date(tempForcast.list[8].dt * 1000).getDay());
+
+  const tempDay2 = tempForcast.list[16].main.temp;
+  const day2 = dayOfWeek(new Date(tempForcast.list[16].dt * 1000).getDay());
+
+  document.getElementById("w-forcast").innerHTML = `
+      Today: ${tempToday}°C <br>
+      ${day}: ${tempTomorrow}°C <br>
+      ${day2}: ${tempDay2}°C`;
+}
+main();
