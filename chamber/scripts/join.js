21eb90d994719610aee7b1f01ec6785cf3cc22c5
Edward Kamwi <157596090+EddyKamwi@users.noreply.github.com>
2025-04-03 00:49:15 +0200

    dialog element

---
+++ chamber/scripts/join.js
@@ -0,0 +1,21 @@
+const nonBtn = document.getElementById("non")
+const bronzeBtn = document.getElementById("bron")
+const silverBtn = document.getElementById("sil")
+const goldBtn = document.getElementById("gol")
+const dialogBox = document.querySelector("dialog")
+const title = document.querySelector(".d-title")
+const content = document.querySelector(".d-content")
+
+nonBtn.addEventListener("click", () => {
+    title.innerText = `Non Profit Membership Level`;
+    dialogBox.showModal();
+}); bronzeBtn.addEventListener("click", () => {
+    title.innerText = `Bronze Membership Level`;
+    dialogBox.showModal();
+}); silverBtn.addEventListener("click", () => {
+    title.innerText = `Silver Membership Level`;
+    dialogBox.showModal();
+}); goldBtn.addEventListener("click", () => {
+    title.innerText = `Gold Membership Level`;
+    dialogBox.showModal();
+});
