eabec655ad2966a22b13166b7ff81b17b861fd8c
Edward Kamwi <157596090+EddyKamwi@users.noreply.github.com>
2025-04-04 01:44:47 +0200

    discover photos

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,5 +1,10 @@
 async function main() {
-    
-const data = await fetch("https://eddykamwi.github.io/wdd231/chamber/data/interests.json")
-    
+
+    const data = await fetch("https://eddykamwi.github.io/wdd231/chamber/data/interests.json")
+    const results = data.json()
+    results.then(interests => {
+        console.log(interests[0].name)
+    })
+
 }
+main()
