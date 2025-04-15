b90897a4dad72d78f902df5a0382834e73fac2d3
Edward Kamwi <157596090+EddyKamwi@users.noreply.github.com>
2025-04-15 22:54:29 +0200

    intergreting the dogs ceo api

---
+++ dogs/scripts/dogsGallery.js
@@ -0,0 +1,28 @@
+const dog = async () => {
+  const results = (
+    await fetch("https://dog.ceo/api/breeds/image/random")
+  ).json();
+
+    results.then((data) => {
+      
+    const parts = data.message.split("/").filter((part) => part !== "");
+
+    // Find the index of 'breeds' in the filtered array
+    const breedsIndex = parts.indexOf("breeds");
+
+    let breedName = "Breed not found";
+    if (breedsIndex !== -1 && breedsIndex + 1 < parts.length) {
+      breedName = parts[breedsIndex + 1];
+    }
+
+    const container = document.querySelector(".gallery");
+    const div = document.createElement("div");
+    div.setAttribute("class", "image-card");
+    div.innerHTML = `<span class="dog-name">${breedName}</span><img src="${data.message}" id="random-dog" class="image-item" width="320" height="420" alt="Random dog" loading="lazy">`;
+    container.appendChild(div);
+  });
+};
+// Call the function to display a random dog image
+for (let i = 0; i < 6; i++) {
+  dog();
+}
--- dogs/scripts/main.js
+++ dogs/scripts/main.js
@@ -1,4 +1,5 @@
 const navBtn = document.getElementById("menu-bars");
+
 const toggleNav = () => {
   const nav = document.querySelector("nav");
   nav.classList.toggle("show");
@@ -6,3 +7,9 @@ const toggleNav = () => {
 };
 
 navBtn.addEventListener("click", toggleNav);
+
+//display the current year in the footer
+document.getElementById("year").innerHTML = new Date().getFullYear();
+//display the last modified date in the footer
+document.getElementById("lastmodified").innerHTML = document.lastModified;
+
