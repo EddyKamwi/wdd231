d1021c164de6350f65f01a1807d907ee21b72d7a
Edward Kamwi <157596090+EddyKamwi@users.noreply.github.com>
2025-04-21 07:41:22 +0200

    fixed the url

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -48,7 +48,7 @@ const displayCompany = (array) => {
   });
 };
 const data = async () => {
-  const url = "http://127.0.0.1:5500/chamber/data/members.json";
+  const url = "https://eddykamwi.github.io/chamber/data/members.json";
   const resp = await fetch(url);
   const data = await resp.json();
   displayCompany(data.companies);
