3a896a84cb84c762d203dba71caf79bbbda592fc
Edward Kamwi <157596090+EddyKamwi@users.noreply.github.com>
2025-04-21 08:21:23 +0200

    doing some corrections

--- chamber/scripts/thanks.js
+++ chamber/scripts/thanks.js
@@ -1,18 +1,21 @@
-const params = window.location.search
-const data = new URLSearchParams(params)
-const element = document.getElementById("f-results")
+const params = window.location.search;
+const data = new URLSearchParams(params);
+const element = document.getElementById("f-results");
 
 /**
- * thank you page loads upon successful form submission. The thank you 
- * page displays the first name, last name, email address, phone number, 
- * business/organization name, and the date from the hidden input. The design 
+ * thank you page loads upon successful form submission. The thank you
+ * page displays the first name, last name, email address, phone number,
+ * business/organization name, and the date from the hidden input. The design
  * follows the site pattern.
  */
-element.innerText=`
-First Name:     ${data.get("firstname")}
-Last Name:      ${data.get("lastname")}
-Email:          ${data.get("email")}
-Phone Number:   ${data.get("phone")}
-Business Name:  ${data.get("businessname")}
-Date:           ${new Date(parseInt(data.get("timestamp")))}
-`
+element.innerHTML = `
+    <h2>Your Details</h2><br><hr><br>
+    <div>
+        <p>First Name:     ${data.get("firstname")}</p>
+        <p>Last Name:      ${data.get("lastname")}</p>
+        <p>Email:          ${data.get("email")}</p>
+        <p>Phone Number:   ${data.get("phone")}</p>
+        <p>Business Name:  ${data.get("businessname")}</p>
+        <p>Date:           ${new Date(parseInt(data.get("timestamp")))}</p>
+    </div>
+`;
